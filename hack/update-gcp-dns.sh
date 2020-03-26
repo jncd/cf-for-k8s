6ef1f4a7dd91a0919c7eae86426a4b73c2052bc1
James Pollard <jpollard@pivotal.io>
2020-03-26 11:49:03 -0700

    STYLE: nicely and consistently format bash files

--- hack/update-gcp-dns.sh
+++ hack/update-gcp-dns.sh
@@ -10,8 +10,8 @@ if [ $# -lt 2 ]; then
 fi
 
 # Ensure that required executables exist
-gcloud --version > /dev/null 2>&1 || (echo "Missing required \"gcloud\" executable." && exit 1)
-kubectl version --client=true > /dev/null 2>&1 || (echo "Missing required \"kubectl\" executable." && exit 1)
+gcloud --version >/dev/null 2>&1 || (echo "Missing required \"gcloud\" executable." && exit 1)
+kubectl version --client=true >/dev/null 2>&1 || (echo "Missing required \"kubectl\" executable." && exit 1)
 
 DNS_DOMAIN="$1"
 DNS_ZONE_NAME="$2"
@@ -22,11 +22,11 @@ external_static_ip=$(kubectl get services/istio-ingressgateway -n istio-system -
 echo "Starting transaction..."
 gcloud dns record-sets transaction start --zone="${DNS_ZONE_NAME}"
 
-gcp_records_json="$( gcloud dns record-sets list --zone="${DNS_ZONE_NAME}" --name "*.${DNS_DOMAIN}" --format=json )"
-record_count="$( echo "${gcp_records_json}" | jq 'length' )"
+gcp_records_json="$(gcloud dns record-sets list --zone="${DNS_ZONE_NAME}" --name "*.${DNS_DOMAIN}" --format=json)"
+record_count="$(echo "${gcp_records_json}" | jq 'length')"
 if [ "${record_count}" != "0" ]; then
-	echo "Deleting existing DNS A record..."
-  existing_record_ip="$( echo "${gcp_records_json}" | jq -r '.[0].rrdatas | join(" ")' )"
+  echo "Deleting existing DNS A record..."
+  existing_record_ip="$(echo "${gcp_records_json}" | jq -r '.[0].rrdatas | join(" ")')"
   gcloud dns record-sets transaction remove --name "*.${DNS_DOMAIN}" --type=A --zone="${DNS_ZONE_NAME}" --ttl=5 "${existing_record_ip}" --verbosity=debug
 fi
 
